1d20bd4f2987fe56e8142d61f0fccd9debfeda82
Azka Fadhli Ramadhan <azkafr92@gmail.com>
2023-04-08 16:30:13 +0700

    feat: division and multiplication features done

--- py/test_money.py
+++ py/test_money.py
@@ -1,20 +1,36 @@
 import unittest
 
 
-class Dollar:
-    def __init__(self, amount) -> None:
+class Money:
+    def __init__(self, amount, currency) -> None:
         self.amount = amount
+        self.currency = currency
     
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+    
+    def divide(self, divider):
+        return Money(self.amount / divider, self.currency)
+    
+    def __eq__(self, __value) -> bool:
+        return self.amount == __value.amount and self.currency == __value.currency
 
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def testMultiplicationInDollars(self):
+        fiver = Money(5, "USD")
+        tenner = Money(10, "USD")
+        self.assertEqual(fiver.times(2), tenner)
 
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = Money(20, "EUR")
+        self.assertEqual(tenEuros.times(2), twentyEuros)
+    
+    def testDivision(self):
+        originalMoney = Money(4002, "KRW")
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(originalMoney.divide(4), expectedMoneyAfterDivision)
 
 if __name__ == "__main__":
     unittest.main()
